00274abde8b04fa26244200dc0ca1d787b95392a
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-10-18 00:05:36 -0300

    new: add Module Math with function sum, sub and mult

---
+++ math.exs
@@ -0,0 +1,15 @@
+defmodule Math do
+  def sum(a, b) do
+    a + b
+  end
+  def sub(a, b) do
+    a - b
+  end
+  def mult(a, b) do
+    a * b
+  end
+end
+
+IO.puts Math.sum(1, 2)
+IO.puts Math.sub(5, 1)
+IO.puts Math.mult(5, 2)
