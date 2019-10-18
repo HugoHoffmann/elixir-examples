4366d3326b1ffddfca366be03e9cc4996bd0423f
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-10-18 00:07:09 -0300

    feat(div): add new function to module Math

--- math.exs
+++ math.exs
@@ -8,8 +8,13 @@ defmodule Math do
   def mult(a, b) do
     a * b
   end
+  def div(a, b) do
+    a / b
+  end
+
 end
 
 IO.puts Math.sum(1, 2)
 IO.puts Math.sub(5, 1)
 IO.puts Math.mult(5, 2)
+IO.puts Math.div(5, 2)
